eccaee75219c23b565f59ff9b54a09f67ba636a7
GiovanniPoggi <giovanni.poggi97@studio.unibo.it>
2018-06-23 22:22:13 +0200

    ADD on Model, conn a DB su myphpadmin(da cambiare num porta se si prova)

--- src/controller/MainApp.java
+++ src/controller/MainApp.java
@@ -20,7 +20,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Emittente TV.");
+        this.primaryStage.setTitle("Emittente TV");
 
         initRootLayout();
 
---
+++ src/model/DBConnect.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnect {
+	
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+
+	public DBConnect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/progettodb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+			st = con.createStatement();
+			
+		} catch (Exception ex) {
+			System.out.println("Error " +ex);
+		}
+	}
+	
+	public void getData() {
+		try {
+			String query = "select * from film";
+			rs = st.executeQuery(query);
+			System.out.println("Records from database");
+			while (rs.next()) {
+				String idfilm = rs.getString("idFilm");
+				System.out.println("ID: "+idfilm);
+			}
+			
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+}
